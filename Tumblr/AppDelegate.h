e3196fa5e317d53d8002ca865d1ef73b145bfd53
Santino L Ramos <santinoramos@fb.com>
2021-06-24 11:05:36 -0400

    Initial Commit

---
+++ Tumblr/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tumblr
+//
+//  Created by Santino L Ramos on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tumblr/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Tumblr
+//
+//  Created by Santino L Ramos on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
